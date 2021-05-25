e83b6e3a0faf08e6448cf614561eb3355e91ac53
Alax Gabriel <alaxgabriel@ufrn.edu.br>
2021-05-25 19:27:33 -0300

    :pencil: editing Auncio.js

--- models/Anuncio.js
+++ models/Anuncio.js
@@ -9,6 +9,32 @@ const Anuncio = new mongoose.Schema({
         type: String,
         required: true
     },
+    image: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    contact: {
+        number:{
+            type: String,
+            required: true
+        },
+        email:{
+            type: String,
+            required: false
+        },
+        instagram: {
+            type: String,
+            required: false
+        },
+        facebook: {
+            type: String,
+            required: false
+        }
+    },
     category: {
         type: String,
         required: true
